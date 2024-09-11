09a55863b58b58ac655012f02920a94cd42cb36d
Konstantinos Kazazis <kazaziskonstantinos@gmail.com>
2024-09-11 22:31:45 +0200

    loading spinner in home relative to getresumes and expired tokens

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,8 +7,10 @@ import { Header } from "../../components/layout/index";
 import { Footer } from "../../components/layout/index";
 import SavedResumes from "../../components/SavedResumes";
 import Plain from "../../components/templates/Plain";
+import LoadingSpinner from "../../components/LoadingSpinner";
 
 const Home = () => {
+  const [loading, setLoading] = useState<boolean>(false);
   const [token, setToken] = useState<string | null>(getAccessToken());
   const [userId, setUserId] = useState<string | null>(
     localStorage.getItem("User")
@@ -20,18 +22,21 @@ const Home = () => {
   useEffect(() => {
     const getResumes = async () => {
       try {
+        setLoading(true);
         const response = await axios.get(`/api/resumes`, {
           headers: { Authorization: `Bearer ${token}` },
         });
 
         const fetchedResumes = response.data.resumes;
         setResumes(fetchedResumes);
+        setLoading(false);
       } catch (error) {
         console.error("Get Resumes Error:", error.message);
 
         if (error.response?.status === 403 || error.response?.status === 401) {
           localStorage.removeItem("accessToken");
           window.location.reload();
+          setLoading(false);
           console.log("Token expired or invalid, please log in again.");
         }
       }
@@ -75,63 +80,74 @@ const Home = () => {
 
   return (
     <div>
-      <Header />
-      <div className="flex h-screen p-8">
-        <div className="w-[40%] overflow-auto mt-12">
-          <SavedResumes resumes={resumes} onResumeClick={handleResumeClick} />
-        </div>
-        <div className="flex-1 float-left overflow-y-auto bg-slate-100 hover:opacity-50 transition-transform duration-200 cursor-pointer mt-12">
-          <Link to="/create">
-            <div onClick={setResumeIdfun}>
-              {selectedResume ? (
-                <Plain
-                  fullname={selectedResume.fullname}
-                  title={selectedResume.title}
-                  email={selectedResume.email}
-                  phone={selectedResume.phone}
-                  linkedin={selectedResume.linkedin}
-                  portfolio={selectedResume.portfolio}
-                  country={selectedResume.country}
-                  repos={selectedResume.repos}
-                  workExperiences={selectedResume.workExperiences}
-                  projects={selectedResume.projects}
-                  education={selectedResume.education}
-                  languages={selectedResume.languages}
-                  skills={selectedResume.skills}
-                />
-              ) : resumes.length > 0 ? (
-                <Plain
-                  fullname={resumes[latestResumeIndex].fullname}
-                  title={resumes[latestResumeIndex].title}
-                  email={resumes[latestResumeIndex].email}
-                  phone={resumes[latestResumeIndex].phone}
-                  linkedin={resumes[latestResumeIndex].linkedin}
-                  portfolio={resumes[latestResumeIndex].portfolio}
-                  country={resumes[latestResumeIndex].country}
-                  repos={resumes[latestResumeIndex].repos}
-                  workExperiences={resumes[latestResumeIndex].workExperiences}
-                  projects={resumes[latestResumeIndex].projects}
-                  education={resumes[latestResumeIndex].education}
-                  languages={resumes[latestResumeIndex].languages}
-                  skills={resumes[latestResumeIndex].skills}
-                />
-              ) : (
-                <div className="flex justify-center pt-20">
-                  <div>
-                    <h1 className="text-3xl text-center opacity-30">
-                      No resumes found
-                    </h1>
-                    <p className="text-xl opacity-30">
-                      Please press the "+" button to create a new one.
-                    </p>
-                  </div>
-                </div>
-              )}
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <Header />
+          <div className="flex h-screen p-8">
+            <div className="w-[40%] overflow-auto mt-12">
+              <SavedResumes
+                resumes={resumes}
+                onResumeClick={handleResumeClick}
+              />
             </div>
-          </Link>
-        </div>
-      </div>
-      <Footer />
+            <div className="flex-1 float-left overflow-y-auto bg-slate-100 hover:opacity-50 transition-transform duration-200 cursor-pointer mt-12">
+              <Link to="/create">
+                <div onClick={setResumeIdfun}>
+                  {selectedResume ? (
+                    <Plain
+                      fullname={selectedResume.fullname}
+                      title={selectedResume.title}
+                      email={selectedResume.email}
+                      phone={selectedResume.phone}
+                      linkedin={selectedResume.linkedin}
+                      portfolio={selectedResume.portfolio}
+                      country={selectedResume.country}
+                      repos={selectedResume.repos}
+                      workExperiences={selectedResume.workExperiences}
+                      projects={selectedResume.projects}
+                      education={selectedResume.education}
+                      languages={selectedResume.languages}
+                      skills={selectedResume.skills}
+                    />
+                  ) : resumes.length > 0 ? (
+                    <Plain
+                      fullname={resumes[latestResumeIndex].fullname}
+                      title={resumes[latestResumeIndex].title}
+                      email={resumes[latestResumeIndex].email}
+                      phone={resumes[latestResumeIndex].phone}
+                      linkedin={resumes[latestResumeIndex].linkedin}
+                      portfolio={resumes[latestResumeIndex].portfolio}
+                      country={resumes[latestResumeIndex].country}
+                      repos={resumes[latestResumeIndex].repos}
+                      workExperiences={
+                        resumes[latestResumeIndex].workExperiences
+                      }
+                      projects={resumes[latestResumeIndex].projects}
+                      education={resumes[latestResumeIndex].education}
+                      languages={resumes[latestResumeIndex].languages}
+                      skills={resumes[latestResumeIndex].skills}
+                    />
+                  ) : (
+                    <div className="flex justify-center pt-20">
+                      <div>
+                        <h1 className="text-3xl text-center opacity-30">
+                          No resumes found
+                        </h1>
+                        <p className="text-xl opacity-30">
+                          Please press the "+" button to create a new one.
+                        </p>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </Link>
+            </div>
+          </div>
+          <Footer />
+        </>
+      )}
     </div>
   );
 };
