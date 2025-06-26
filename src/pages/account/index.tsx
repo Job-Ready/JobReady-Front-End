b6fd732c767d8cf7f9258de8f9d456d3f4f5cfc1
Konstantinos Kazazis <kazaziskonstantinos@gmail.com>
2025-06-26 15:08:49 +0200

    token fix

---
+++ src/pages/account/index.tsx
@@ -0,0 +1,20 @@
+import { Header } from "../../components/layout/index";
+import { Footer } from "../../components/layout/index";
+import {useState} from "react";
+import {getAccessToken} from "@utils/auth";
+import {Navigate} from "react-router-dom";
+
+const Account: React.FC = () => {
+    const [token, setToken] = useState<string | null>(getAccessToken());
+    if (!token) {
+        return <Navigate replace to="/" />;
+    }
+    return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default Account;
