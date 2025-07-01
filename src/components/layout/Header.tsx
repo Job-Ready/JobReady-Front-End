cc4a7d5cba293b807194d24ab13e8ee5dc77194a
Konstantinos Kazazis <kazaziskonstantinos@gmail.com>
2025-07-01 16:40:25 +0200

    delete profile pic from header acordion

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -145,11 +145,6 @@ const Header: React.FC = () => {
                             href="#"
                             className="flex items-center p-3 -mt-2 text-sm text-gray-600 transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
                           >
-                            <img
-                              className="flex-shrink-0 object-cover mx-1 rounded-full w-9 h-9"
-                              src="https://images.unsplash.com/photo-1523779917675-b6ed3a42a561?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8d29tYW4lMjBibHVlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=face&w=500&q=200"
-                              alt="jane avatar"
-                            />
                             <div className="mx-1">
                               <h1 className="text-sm font-semibold text-gray-700 dark:text-gray-200">
                                 {username}
